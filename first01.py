36b936130b5d5b5a4c010db036e428bdf4da0236
Banthita22 <164161@wsk.ac.th>
2026-06-16 04:19:25 +0000

    first01.py M

--- first01.py
+++ first01.py
@@ -1,2 +1,6 @@
-print("Hello world")
-print
+print("โปรแกรมคํษนวณหาพื้นที่สามเหลี่ยม")
+h=float(input("enter hight:"))
+b=float(input("enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("จัดทําโดย นางสาว บัณฑิตา ใจมูล เลขที่22")
